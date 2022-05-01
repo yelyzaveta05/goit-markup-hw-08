fcfd1390d0065ed02a361f7712e342530de7e89f
Yelyzaveta Demchenko <97131904+yelyzaveta05@users.noreply.github.com>
2022-05-01 21:17:55 +0300

    hero

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,14 @@
-const refs = {
-  openModalBtn: document.querySelectorAll("[data-menu-open]"),
-  closeModalBtn: document.querySelector("[data-menu-close]"),
-  modal: document.querySelector("[data-menu]"),
-};
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-refs.openModalBtn.forEach((el) => {
-  el.addEventListener("click", toggleModal);
-});
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  refs.modal.classList.toggle("is-open");
-}
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
+})();
